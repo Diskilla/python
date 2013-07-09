822e06509f8865229c73bfe7bc4d39279a5a7b0b
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-07-09 09:07:05 +0200

    implementieren der main Funktion. einige subroutienen, die erwähnt werden, sind noch nicht implementiert

--- GameOfLife.py
+++ GameOfLife.py
@@ -13,3 +13,29 @@ def initLivingSpace():
         livingSpace.append([])
         for y in range(livingSpaceHeight):
             livingSpace[x].append(random.randint(0,1))
+
+def main():
+    video_flags = OPENGL | HWSURFACE | DOUBLEBUF
+    screenSize = (livingSpaceWidth * creatureSize,
+     livingSpaceHeight * creatureSize)
+
+    pygame.init()
+    pygame.display.set_mode(screenSize, video_flags)
+
+    initLivingSpace()
+    resize(screenSize)
+    init()
+
+    frames = 0
+    ticks = pygame.time.get_ticks()
+    while True:
+        event = pygame.event.poll()
+        if event.type == QUIT or
+         (event.type == KEYDOWN and event.key == K_ESCAPE):
+             break
+
+         draw()
+         calculateNextGeneration()
+         pygame.display.flip()
+
+IF __name__ == '__main__': main()
