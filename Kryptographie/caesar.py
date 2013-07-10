10b4e9939f2555f6479bdd199e4db33eb44bc293
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-07-10 14:34:03 +0200

    Funktion zur Verschlüsselung nach Caesar-Chiffre implementieren
    Die Caesar-Chiffre ist eine der simpelsten Kryptographie-Methoden.
    Dabei wird das aktuelle Zeichen einfach gegen ein anderes ausgetauscht.
    Welches das neue Zeichen ist, wird durch den Schlüssel festgelegt. Der
    Schlüssel ist die Anzahl, um die in der Liste der Buchstaben (z.B. a-z)
    verschoben wird.

--- Kryptographie/caesar.py
+++ Kryptographie/caesar.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python
 # Caesar-Chiffre implementierung
 
+def caesar(text, key):
+    cipher = ""
+    for letter in text:
+        # index des aktuellen Klartextbuchstabens im Symbolvorrat bestimmen
+        index   = symbols.index(letter)
+        # Addition des Schluessels zum Index (um die Laenge des Index verschieben)
+        # und Sicherstellung, dass der Index im gueltigen Bereich bleibt
+        index   = (index + key) % len(symbols)
+        cipher += symbols[index]
+
+    return cipher
