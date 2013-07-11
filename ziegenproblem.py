b43fd0442853359d4ea108d538c4cb962a96ccfd
Josef Florian Sedlmeier <josef.sedlmeier@exirius.de>
2013-07-11 13:55:37 +0200

    Klasse Player implementieren
    Die Klasse Player hat die beiden Attribute 'guess' und 'wins'. In
    'guess' wird das aktuell geratene Tor gespeichert. In 'wins' wird die
    Anzahl der richtig erkannten Tore gespeichert. Ebenso hat die Klasse 3
    methoden. 'newGuess' gibt eine neue Torspekulation zurück, 'changeMind'
    ändert das attribut 'guess' wenn sich die Torspekulation ändert und
    'saveResult' zählt im Falle eines richtig geratenen Tores den 'wins'
    Counter hoch.

--- ziegenproblem.py
+++ ziegenproblem.py
@@ -4,3 +4,21 @@
 # Autor: Josef Florian Sedlmeier
 
 import random
+
+class Player(object):
+    def __init__(self):
+        self.guess = 0
+        self.wins  = 0
+
+    def newGuess(self):
+        self.guess = random.randint(1, 3)
+
+    def changeMind(self, knownNumber):
+        allNumbers = set((1, 2, 3))
+        allNumbers.remove(self.guess)
+        allNumbers.remove(knownNumber)
+        self.guess = allNumbers.pop()
+
+    def saveResult(self, winning):
+        if winning == self.guess:
+            self.win += 1
